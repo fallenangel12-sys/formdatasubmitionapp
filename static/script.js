616dc180802c98df21eddfc6a55bfb64bc6528ca
jalen Davis <jalendavis@TheHiddProject7.hsd1.ga.comcast.net>
2024-12-17 17:27:24 -0500

    first commit

---
+++ static/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded',() =>(
+    let name = document.getElementById("name").value
+    let email = document.getElementById("email").value
+    let message = document.getElementById("message").value
+))
